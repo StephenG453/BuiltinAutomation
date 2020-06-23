4769b56cf576541a786720f169d8142880c17b88
StephenG98 <txsteven@gmail.com>
2020-06-23 14:27:25 -0500

    test commit

---
+++ UIAutomation/src/main/java/pages/HomePage.java
@@ -0,0 +1,28 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import java.util.concurrent.TimeUnit;
+
+public class HomePage extends WebDriverInitiation {
+
+    @FindBy(xpath = "//div[@class='block block-builtin-frontpage-trending-articles']//div[@class='block-content']//div[1]")
+    private WebElement welcomeText;
+
+    @FindBy(xpath = "//a[@class='employers']")
+    private WebElement postJobButton;
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean isAtHomePage() {
+        return welcomeText.isDisplayed();
+    }
+
+    public void clickPostJobButton() {
+        postJobButton.click();
+    }
+}
